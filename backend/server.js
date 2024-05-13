0612bca270dfdea64011dd8dec10c574157e6399
Garrett <garrettswink421@gmail.com>
2024-05-13 14:10:49 -0400

    Routed to database; error handling; controllers; route updates

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv'
 import cors from 'cors';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 3001;
 
@@ -20,14 +21,10 @@ app.get('/', (req, res) => {
     res.send('API is running');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-})
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
