f2337ff56aad761edba3d620a901cf0995a3f748
Garrett <garrettswink421@gmail.com>
2024-05-17 13:30:19 -0400

    user routes and controller; email + password auth

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 3001;
@@ -17,11 +18,16 @@ app.use(cors({
     credentials: true,
 }));
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('API is running');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
