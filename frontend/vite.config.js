5c4d3489a3d2be7b299fd39fc22bcdf51fe92f90
Garrett <garrettswink421@gmail.com>
2024-05-28 15:16:57 -0400

    Revert Vite Config For Now

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // proxy requests prefixed '/api' and '/uploads'
-    // Added proxies per Traversy input.
-    // Doesn't seem effective, as still need credentials: 'include' to handle the cookie
-    // Keeping this for now, in case I really run into trouble with deployment
-
-    proxy: {
-      '/api': 'http://localhost:8000',
-      '/uploads': 'http://localhost:5000',
-    },
-  },
-});
+})
