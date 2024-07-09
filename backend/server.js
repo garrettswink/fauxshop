cc124c7ccfdefd71a7bd3ce7d85bf192e1c0f024
Garrett <garrettswink421@gmail.com>
2024-07-09 17:09:35 -0400

    updates

--- backend/server.js
+++ backend/server.js
@@ -49,15 +49,17 @@ app.use('/images', express.static(path.join(__dirname, '..', 'frontend', 'public
 // app.use('/public/uploads/', express.static(path.join(__dirname, '/public/uploads/')));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname/build)));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
 
     app.get('*', (req,res) => 
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-);
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
 } else {
+
     app.get('/', (req, res) => {
         res.send('API is running');
     });
+    
 }
 
 
