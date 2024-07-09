f88ddd9321e92cc37c3f653787ba637cb47a778f
Garrett <garrettswink421@gmail.com>
2024-07-09 10:18:39 -0400

    Final Updates

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running');
-});
+
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -49,6 +47,19 @@ const __dirname = path.resolve(); // Set __dirname to current directory
 app.use('/images', express.static(path.join(__dirname, '..', 'frontend', 'public', 'images')));
 // Original of the above. I think the above is better. Might need to swap out.
 // app.use('/public/uploads/', express.static(path.join(__dirname, '/public/uploads/')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname/build)));
+
+    app.get('*', (req,res) => 
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+);
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running');
+    });
+}
+
 
 app.use(notFound);
 app.use(errorHandler);
