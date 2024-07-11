cbb61e1bd589920496876d47e2c3cd02ea8c195d
Garrett <garrettswink421@gmail.com>
2024-07-11 09:30:39 -0400

    Updated server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 dotenv.config();
@@ -9,9 +9,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js'
-
-
+import uploadRoutes from './routes/uploadRoutes.js';
 
 const port = process.env.PORT || 3001;
 
@@ -19,9 +17,22 @@ connectDB(); // Connect to MongoDB
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://fauxshop.onrender.com'
+];
+
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
 }));
 
 // Body parser middleware
@@ -30,8 +41,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // Cookie parser middleware
 app.use(cookieParser());
-
-
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -45,8 +54,6 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve(); // Set __dirname to current directory
 
 app.use('/images', express.static(path.join(__dirname, '..', 'frontend', 'public', 'images')));
-// Original of the above. I think the above is better. Might need to swap out.
-// app.use('/public/uploads/', express.static(path.join(__dirname, '/public/uploads/')));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
@@ -55,18 +62,12 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
     );
 } else {
-
     app.get('/', (req, res) => {
         res.send('API is running');
     });
-    
 }
-
 
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-
-
