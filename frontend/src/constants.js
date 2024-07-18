f408be82c777e487272c5c6f6de53a05082ddebc
Garrett <garrettswink421@gmail.com>
2024-07-18 12:29:05 -0400

    Updated Base URL

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -14,8 +14,7 @@
 
 // Depoloyment Constants
 
-const BASE_URL = 'https://fauxshop.onrender.com';
-
+export const BASE_URL = 'https://fauxshop.onrender.com';
 export const PRODUCTS_URL = `${BASE_URL}/api/products`;
 export const USERS_URL = `${BASE_URL}/api/users`;
 export const ORDERS_URL = `${BASE_URL}/api/orders`;
